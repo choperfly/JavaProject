cc9f204847748b99bf4597e46c8f7aada1e09780
Manuel Ley <choperfly@hotmail.com>
2021-08-23 12:12:44 -0500

    test

---
+++ src/com/java/main/Main.java
@@ -0,0 +1,36 @@
+package com.java.main;
+
+import com.java.basicprograms.*;
+
+public class Main {
+    public static void main(String[] args) {
+       basicPrograms();
+    }
+
+    private static void basicPrograms(){
+        //***Exercise#1 - Fibonacci example
+        //Fibonacci.FibonacciExample();
+
+        ////***Exercise#2 - Prime number
+        /*for (int i = 0; i<11;i++ ) {
+            PrimeNumber.primeNumber(i);
+        }*/
+
+        //***Exercise#3 - Palindrome
+        //PalindromeNumber.palindromeNumber(1012);
+        //PalindromeNumber.palindromeNumber(121);
+
+        //***Exercise#4 - Factorial Number
+        //FactorialNumber.getFactorialNumber(5);
+
+        //***Exercise#5 - Factorial Number
+        //ArmstrongNumber.isAmrstrongNumber(2000);
+
+        //***Exercise #6 Random number
+        RandomNumber.printRandomMath();
+
+        //**Exercise #7 Patterns
+        //Patterns.printTrianglePattern();
+
+    }
+}
